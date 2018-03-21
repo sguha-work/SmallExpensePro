d1f194cb475f94e9a3c8b5d7f457ac79aa18170a
sguha-work <sguha1988.life@gmail.com>
2018-03-21 19:51:38 +0530

    service file added and requirement changes

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,32 +1,33 @@
-import {Http} from '@angular/http';
-import {Injectable} from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 
+let firebase;
 @Injectable()
 export class Database {
-   constructor(private db: AngularFireDatabase) {
-   }
+    private db: any;
+    constructor() {
+        this.db = firebase.database();
+    }
 
-   public writeToDatabase(key: string, data: any): Promise<any> {
-       return new Promise((resolve, reject) => {
-        this.db.database.ref('/'+key).set(data).then(() => {
-            resolve();
-        }).catch((error) => {
-            reject();
+    public writeToDatabase(key: string, data: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.db.database.ref('/' + key).set(data).then(() => {
+                resolve();
+            }).catch((error) => {
+                reject();
+            });
         });
-       });
-       
-   }
 
-   public getFromDatabase(key: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-        this.db.database.ref('/'+key).once('value').then((data) => {
-            resolve(data.val());
-        }).catch((error) => {
-            reject(error);
+    }
+
+    public getFromDatabase(key: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.db.database.ref('/' + key).once('value').then((data) => {
+                resolve(data.val());
+            }).catch((error) => {
+                reject(error);
+            });
         });
-    });
-   }
-   
+    }
+
 }
