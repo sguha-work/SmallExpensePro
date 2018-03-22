78d7ab6abc62452c6b14e90cea94bc99da990323
sguha-work <sguha1988.life@gmail.com>
2018-03-22 13:07:52 +0530

    services attached

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,26 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Events } from 'ionic-angular';
 import { MyApp } from './app.component';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import {File} from '@ionic-native/file';
+import {HttpModule} from '@angular/http';
+import { DatePicker } from '@ionic-native/date-picker';
+import { EmailComposer } from '@ionic-native/email-composer';
+
+// importing self created services
+import { FileHandeler } from './../services/filehandeler.service';
+import { Common } from './../services/common.service';
+import { Database } from './../services/database.service';
+import { Alert } from './../services/alert.service';
+import { Expense } from './../services/expense.service';
+import { TagService } from './../services/tag.service';
+import { NumberService } from './../services/number.service';
+
+// importing self created pages
 import { SmallExpenseTrackerPage } from '../pages/small-expense-tracker/small-expense-tracker';
 import { HistoryPage } from '../pages/history/history';
 import { AboutPage } from '../pages/about/about';
@@ -11,8 +30,7 @@ import { SignupPage } from '../pages/signup/signup';
 
 
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 let pages = [
   MyApp,
@@ -54,7 +72,19 @@ export function entryComponents() {
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    File,
+    EmailComposer,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TagService,
+    NumberService,
+    FileHandeler,
+    Common,
+    Database,
+    Expense,
+    Alert,
+    DatePicker,
+    Events
   ]
 })
 
--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,16 +1,16 @@
-// import {Http} from '@angular/http';
-// import {Injectable} from '@angular/core';
-// import { EmailComposer } from '@ionic-native/email-composer';
-// @Injectable()
-// export class Email {
+import {Http} from '@angular/http';
+import {Injectable} from '@angular/core';
+import { EmailComposer } from '@ionic-native/email-composer';
+@Injectable()
+export class Email {
     
-//     private email: string;
+    private email: string;
     
-//     constructor(public http: Http) {
-//         this.email = "sguha1988.life@gmail.com";
-//     }
+    constructor(public http: Http) {
+        this.email = "sguha1988.life@gmail.com";
+    }
 
-//     public open(data: string,email?: string) {
+    public open(data: string,email?: string) {
 
-//     }
-// }
+    }
+}
